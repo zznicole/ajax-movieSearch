8cd09deeabfaf83ebc7a258b8df5dbea8b3855e5
Nicole Zheng <zznicole@hotmail.com>
2020-05-13 00:24:38 +0200

    get data

--- src/ajax.js
+++ src/ajax.js
@@ -1,14 +1,8 @@
-let userInput = $('.search__bar--input');
-// $("search__bar--button").on('click', function() {
-//   $.ajax('http://www.omdbapi.com/?apikey=171b1b73&t=userInput"').done(function (movieData) {
-//     console.log(movieData);
-//   });
-// });
-
-fetch("htttp://omdbapi.com/?apikey=92bc4124&t=userInput")
-  .then(function (response) {
-    return response = response.json();
-    console.log (response);
-  })
-
-
+$(".search__bar--button").on("click", function () {
+  let userInput = $(".search__bar--input").val();
+  $.ajax("http://www.omdbapi.com/?apikey=171b1b73&t=" + userInput).done(
+    function (response) {
+      console.log(response);
+    }
+  );
+});
